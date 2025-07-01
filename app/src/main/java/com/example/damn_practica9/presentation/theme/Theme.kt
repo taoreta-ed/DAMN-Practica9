0343ca9b987a2ecbe5b132de983020ece83a748a
Eduardo Castro <nora.castroeduardo@gmail.com>
2025-07-01 17:08:52 -0600

    Mensaje de tu primer commit

---
+++ app/src/main/java/com/example/damn_practica9/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.damn_practica9.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun DAMNPractica9Theme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
